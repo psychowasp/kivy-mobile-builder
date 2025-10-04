bf6b2b2b9eff710801a78a8dce90fdc9150400cf
PythonSwiftLink <107263226+PythonSwiftLink@users.noreply.github.com>
2025-10-04 02:49:35 +0200

    Add iOS wheel build script and move old scripts
    Introduced maturin_ios_wheels.sh for building Python wheels targeting iOS and iOS simulators. Moved previous maturin build scripts to other_scripts/old_junk for archival.

---
+++ other_scripts/maturin/maturin_ios_wheels.sh
@@ -0,0 +1,95 @@
+
+build_iphoneos() {
+
+    PYTHON_DIR=$1
+    OPENSSL_DIR=$2
+    PY_VERSION=$3
+
+    export OSX_SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
+    export IOS_SDKROOT=$(xcrun --sdk iphoneos --show-sdk-path)
+    export PYTHONDIR="$1/ios-arm64"
+    export PYO3_CROSS_PYTHON_VERSION=$PY_VERSION
+
+    env SDKROOT="$IOS_SDKROOT" \
+    PYO3_CROSS_LIB_DIR="$PYTHONDIR" \
+    OPENSSL_DIR=$OPENSSL_DIR \
+    CARGO_TARGET_AARCH64_APPLE_IOS_RUSTFLAGS="\
+        -C link-arg=-isysroot -C link-arg=$IOS_SDKROOT \
+        -C link-arg=-arch -C link-arg=arm64 \
+        -C link-arg=-miphoneos-version-min=13.0 \
+        -C link-arg=-L \
+        -C link-arg=$PYTHONDIR \
+        -C link-arg=-undefined \$
+        -C link-arg=dynamic_lookup \
+        " \
+    maturin build --release --target aarch64-apple-ios -v
+
+}
+
+build_iphonesimulator_arm64() {
+
+    PYTHON_DIR=$1
+    OPENSSL_DIR=$2
+    PY_VERSION=$3
+
+    export OSX_SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
+    export IOS_SDKROOT=$(xcrun --sdk iphonesimulator --show-sdk-path)
+    export PYTHONDIR="$PYTHON_XC/ios-arm64-simulator"
+    export PYO3_CROSS_PYTHON_VERSION=$PY_VERSION
+
+    env SDKROOT="$IOS_SDKROOT" \
+    PYO3_CROSS_LIB_DIR="$PYTHONDIR" \
+    OPENSSL_DIR=$OPENSSL_DIR \
+    CARGO_TARGET_AARCH64_APPLE_IOS_SIM_RUSTFLAGS="\
+        -C link-arg=-isysroot -C link-arg=$IOS_SDKROOT \
+        -C link-arg=-arch -C link-arg=arm64 \
+        -C link-arg=-miphoneos-version-min=13.0 \
+        -C link-arg=-L \
+        -C link-arg=$PYTHONDIR \
+        -C link-arg=-undefined -C link-arg=dynamic_lookup
+        " \
+    maturin build --release --target aarch64-apple-ios-sim -v
+
+}
+
+build_iphonesimulator_x86_64() {
+
+    PYTHON_DIR=$1
+    OPENSSL_DIR=$2
+    PY_VERSION=$3
+
+    export OSX_SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
+    export IOS_SDKROOT=$(xcrun --sdk iphonesimulator --show-sdk-path)
+    export PYTHONDIR="$PYTHON_XC/ios-x86_64-simulator"
+    export PYO3_CROSS_PYTHON_VERSION=$PY_VERSION
+
+    env SDKROOT="$IOS_SDKROOT" \
+    PYO3_CROSS_LIB_DIR="$PYTHONDIR" \
+    OPENSSL_DIR=$OPENSSL_DIR \
+    CARGO_TARGET_X86_64_APPLE_IOS_RUSTFLAGS="\
+        -C link-arg=-isysroot -C link-arg=$IOS_SDKROOT \
+        -C link-arg=-arch -C link-arg=x86_64 \
+        -C link-arg=-miphoneos-version-min=13.0 \
+        -C link-arg=-L \
+        -C link-arg=$PYTHONDIR \
+        -C link-arg=-undefined -C link-arg=dynamic_lookup" \
+    maturin build --release --target x86_64-apple-ios -v
+
+}
+
+
+ROOT=$PWD
+PYTHON_XC="$ROOT/../Python.xcframework"
+OPENSSL_DIR="/usr/local/Cellar/openssl@3/3.5.2"
+
+cd $2
+
+build_iphoneos $PYTHON_XC $OPENSSL_DIR 3.13
+find . -type f -name "$1*x86_64.whl" -exec mv {} ../$1-cp313-cp313-ios_13_0_arm64_iphoneos.whl \;
+
+build_iphonesimulator_arm64 $PYTHON_XC $OPENSSL_DIR 3.13
+find . -type f -name "$1*x86_64.whl" -exec mv {} ../$1-cp313-cp313-ios_13_0_arm64_iphonesimulator.whl \;
+
+build_iphonesimulator_x86_64 $PYTHON_XC $OPENSSL_DIR 3.13
+find . -type f -name "$1*x86_64.whl" -exec mv {} ../$1-cp313-cp313-ios_13_0_x86_64_iphonesimulator.whl \;
+
