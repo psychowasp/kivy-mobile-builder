9c4144b9164051a78ce453f22127666a4551b814
PythonSwiftLink <107263226+PythonSwiftLink@users.noreply.github.com>
2025-10-05 07:35:28 +0200

    Add SDL2/SDL3 Kivy packaging scripts and configs
    Introduces packaging scripts, pyproject.toml, and README files for SDL2 and SDL3 Kivy components, including support for ANGLE. Adds a build script for creating iOS frameworks and configuration for Python 3.11. These changes facilitate building and distributing SDL frameworks for Kivy projects.

---
+++ other_scripts/sdl/sdl3/build_frameworks.sh
@@ -0,0 +1,76 @@
+
+SDL_VER="3.2.22"
+SDL_IMAGE_VER="3.2.4"
+SDL_MIXER_VER="78a2035cf4cf95066d7d9e6208e99507376409a7"
+SDL_TTF_VER="3.2.2"
+
+XC_BUILD=""
+
+build_framework() {
+
+    LIB=$1
+    PROJECT=$2
+    ARCHIVE_PATH=$3
+    cd $4
+
+    xcodebuild archive \
+        -scheme $LIB \
+        -project $PROJECT \
+        -archivePath $ARCHIVE_PATH/Release-iphoneos \
+        -destination 'generic/platform=iOS' \
+        -configuration Release BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+        SKIP_INSTALL=NO
+
+    xcodebuild archive \
+        -scheme $LIB \
+        -project $PROJECT \
+        -archivePath $ARCHIVE_PATH/Release-iphonesimulator \
+        -destination 'generic/platform=iOS Simulator' \
+        -configuration Release BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+        SKIP_INSTALL=NO
+
+    xcodebuild -create-xcframework \
+        -framework $ARCHIVE_PATH/Release-iphoneos.xcarchive/Products/Library/Frameworks/$LIB.framework\
+        -framework $ARCHIVE_PATH/Release-iphonesimulator.xcarchive/Products/Library/Frameworks/$LIB.framework\
+        -output ../$LIB.xcframework
+
+    cd ..
+}
+
+
+sdl_ttf_extra() {
+    cd $1
+    external/download.sh
+}
+
+
+download_fws() {
+    SDL_FILE="SDL3-$SDL_VER.tar.gz"
+    wget -O $SDL_FILE https://github.com/libsdl-org/SDL/releases/download/release-$SDL_VER/$SDL_FILE
+    tar -xzvf $SDL_FILE
+
+    FILE="SDL3_image-$SDL_IMAGE_VER.tar.gz"
+    wget -O $FILE https://github.com/libsdl-org/SDL_image/releases/download/release-$SDL_IMAGE_VER/$FILE
+    tar -xzvf $FILE
+
+    FILE="SDL3_mixer-$SDL_MIXER_VER.tar.gz"
+    wget -O $FILE https://github.com/libsdl-org/SDL_mixer/archive/$SDL_MIXER_VER/$FILE
+    tar -xzvf $FILE
+
+    FILE="SDL3_ttf-$SDL_TTF_VER.tar.gz"
+    wget -O $FILE https://github.com/libsdl-org/SDL_ttf/releases/download/release-$SDL_TTF_VER/$FILE
+    #https://github.com/libsdl-org/SDL_ttf/releases/download/release-{version}/SDL3_ttf-{version}.tar.gz
+    tar -xzvf $FILE
+}
+
+
+download_fws
+
+build_framework SDL3 Xcode/SDL/SDL.xcodeproj Xcode/SDL/build SDL3-$SDL_VER
+
+build_framework SDL3_image Xcode/SDL_image.xcodeproj Xcode/build SDL3_image-$SDL_IMAGE_VER
+
+build_framework SDL3_mixer Xcode/SDL_mixer.xcodeproj Xcode/build SDL_mixer-$SDL_MIXER_VER
+
+sdl_ttf_extra SDL3_ttf-$SDL_TTF_VER
+build_framework SDL3_ttf Xcode/SDL_ttf.xcodeproj Xcode/build SDL3_ttf-$SDL_TTF_VER
